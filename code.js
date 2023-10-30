5e7b0eef932410de5081053da29cbe2b1fbc81e9
Countmooshroom <benjaminwatts314@gmail.com>
2023-10-30 15:30:09 -0600

    Wrote Search Function

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function augmentingPath(graph, start, end) {
+    return searchForPath(graph, start, end, []);
+}
+
+function searchForPath(graph, current, end, path) {
+    path.push(current);
+    if (current == end) { return path; }
+
+    for (const key in graph[current]) {
+        if (graph.hasOwnProperty(key) && !path.includes(key)) {
+            let x = searchForPath(graph, key, end, [...path]);
+            if (x.length > 0) { return x;}
+        }
+    }
     return [];
 }
