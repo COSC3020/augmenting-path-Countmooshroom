d8c7de8048a0c70eda6e213b4c5d2e707ddbba49
Countmooshroom <benjaminwatts314@gmail.com>
2023-10-31 09:40:48 -0600

    Added new tests

--- code.test.js
+++ code.test.js
@@ -22,3 +22,16 @@ assert(JSON.stringify(augmentingPath(graph, 'foo', 'bar')) == '[]');
 var graph = {'foo': {},
     'bar': {'foo': 1}};
 assert(JSON.stringify(augmentingPath(graph, 'foo', 'foo')) == JSON.stringify(['foo']));
+
+var graph = {'first': {'second': 4, 'fifth': 8},
+    'second': {'fourth': 3, 'first': 2},
+    'third': {'fifth': 1},
+    'fourth': {'third': 6, 'second': 2},
+    'fifth': {'first': 12}};
+assert(JSON.stringify(augmentingPath(graph, 'first', 'third')) == JSON.stringify(['first', 'second', 'fourth', 'third']));
+
+var graph = {'first': {'second': 3, 'third':5},
+    'second': {'first': 2, 'fourth': 7},
+    'third': {'second': 1, 'fourth':8},
+    'fourth': {'third': 6}};
+assert(JSON.stringify(augmentingPath(graph, 'first', 'fourth')) == JSON.stringify(['first', 'second', 'fourth']));
